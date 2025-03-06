cbe9a97820c0ae2b0f0a9c2f2087ea059b94e45f
Nguyen Thien Hung <ngthienhung277@gmail.com>
2025-03-06 15:40:13 +0700

    text-content #2

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -156,7 +156,7 @@ function initJsToggle() {
     }
     button.onclick = (e) => {
       e.preventDefault();
-      
+
       if (!$(target)) {
         return (document.body.innerText = `Không tìm thấy phần tử "${target}"`);
       }
@@ -187,5 +187,27 @@ window.addEventListener("template-loaded", () => {
       const item = link.closest("li");
       item.classList.toggle("navbar__item--active");
     };
+  });
+});
+
+window.addEventListener("template-loaded", () => {
+  const tabsSelector = "prod-tab__item";
+  const contentsSelector = "prod-tab__content";
+
+  const tabActive = `${tabsSelector}--current`;
+  const contentActive = `${contentsSelector}--current`;
+
+  const tabContainers = $$(".js-tabs");
+  tabContainers.forEach((tabContainer) => {
+    const tabs = tabContainer.querySelectorAll(`.${tabsSelector}`);
+    const contents = tabContainer.querySelectorAll(`.${contentsSelector}`);
+    tabs.forEach((tab, index) => {
+      tab.onclick = () => {
+        tabContainer.querySelector(`.${tabActive}`)?.classList.remove(tabActive);
+        tabContainer.querySelector(`.${contentActive}`)?.classList.remove(contentActive);
+        tab.classList.add(tabActive);
+        contents[index].classList.add(contentActive);
+      };
+    });
   });
 });
